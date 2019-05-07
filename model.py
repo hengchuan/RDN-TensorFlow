8ab3ee33925e2f124a867a9125b96637efbcbe1c
Liu Yang <tjuzdhliuyang@hotmail.com>
2019-05-07 17:49:02 +0800

    learning rate decay

--- model.py
+++ model.py
@@ -60,7 +60,7 @@ class RDN(object):
     #     }
 
     #     return weightsS, biasesS
-    
+
     # def RDBParams(self):
     #     weightsR = {}
     #     biasesR = {}
@@ -125,7 +125,7 @@ class RDN(object):
         }
 
         return weightsS, biasesS
-    
+
     def RDBParams(self):
         weightsR = {}
         biasesR = {}
@@ -175,23 +175,6 @@ class RDN(object):
         }
 
         return weightsU, biasesU
-    
-    def build_model(self, images_shape, labels_shape):
-        self.images = tf.placeholder(tf.float32, images_shape, name='images')
-        self.labels = tf.placeholder(tf.float32, labels_shape, name='labels')
-
-        self.weightsS, self.biasesS = self.SFEParams()
-        self.weightsR, self.biasesR = self.RDBParams()
-        self.weightsD, self.biasesD = self.DFFParams()
-        self.weightsU, self.biasesU = self.UPNParams()
-        self.weight_final = tf.Variable(tf.random_normal([self.kernel_size, self.kernel_size, self.c_dim, self.c_dim], stddev=np.sqrt(2.0/9/3)), name='w_f')
-        self.bias_final = tf.Variable(tf.zeros([self.c_dim], name='b_f')),
-        
-        self.pred = self.model()
-        # self.loss = tf.reduce_mean(tf.square(self.labels - self.pred))
-        self.loss = tf.reduce_mean(tf.abs(self.labels - self.pred))
-        self.summary = tf.summary.scalar('loss', self.loss)
-        self.saver = tf.train.Saver()
 
     def UPN(self, input_layer):
         x = tf.nn.conv2d(input_layer, self.weightsU['w_U_1'], strides=[1,1,1,1], padding='SAME') + self.biasesU['b_U_1']
@@ -219,23 +202,6 @@ class RDN(object):
             rdb_concat.append(rdb_in)
 
         return tf.concat(rdb_concat, axis=3)
-    
-    def model(self):
-        F_1 = tf.nn.conv2d(self.images, self.weightsS['w_S_1'], strides=[1,1,1,1], padding='SAME') + self.biasesS['b_S_1']
-        F0 = tf.nn.conv2d(F_1, self.weightsS['w_S_2'], strides=[1,1,1,1], padding='SAME') + self.biasesS['b_S_2']
-
-        FD = self.RDBs(F0)
-
-        FGF1 = tf.nn.conv2d(FD, self.weightsD['w_D_1'], strides=[1,1,1,1], padding='SAME') + self.biasesD['b_D_1']
-        FGF2 = tf.nn.conv2d(FGF1, self.weightsD['w_D_2'], strides=[1,1,1,1], padding='SAME') + self.biasesD['b_D_2']
-
-        FDF = tf.add(FGF2, F_1)
-
-        FU = self.UPN(FDF)
-        # FU = self.UPN(F_1)
-        IHR = tf.nn.conv2d(FU, self.weight_final, strides=[1,1,1,1], padding='SAME') + self.bias_final
-
-        return IHR
 
     # NOTE: train with batch size 
     def _phase_shift(self, I, r):
@@ -267,41 +233,90 @@ class RDN(object):
             X = tf.concat([self._phase_shift_test(x, r) for x in Xc], 3) # Do the concat RGB
         return X
 
+    def model(self):
+        F_1 = tf.nn.conv2d(self.images, self.weightsS['w_S_1'], strides=[1,1,1,1], padding='SAME') + self.biasesS['b_S_1']
+        F0 = tf.nn.conv2d(F_1, self.weightsS['w_S_2'], strides=[1,1,1,1], padding='SAME') + self.biasesS['b_S_2']
+
+        FD = self.RDBs(F0)
+
+        FGF1 = tf.nn.conv2d(FD, self.weightsD['w_D_1'], strides=[1,1,1,1], padding='SAME') + self.biasesD['b_D_1']
+        FGF2 = tf.nn.conv2d(FGF1, self.weightsD['w_D_2'], strides=[1,1,1,1], padding='SAME') + self.biasesD['b_D_2']
+
+        FDF = tf.add(FGF2, F_1)
+
+        FU = self.UPN(FDF)
+        # FU = self.UPN(F_1)
+        IHR = tf.nn.conv2d(FU, self.weight_final, strides=[1,1,1,1], padding='SAME') + self.bias_final
+
+        return IHR
+
+    def build_model(self, images_shape, labels_shape):
+        self.images = tf.placeholder(tf.float32, images_shape, name='images')
+        self.labels = tf.placeholder(tf.float32, labels_shape, name='labels')
+
+        self.weightsS, self.biasesS = self.SFEParams()
+        self.weightsR, self.biasesR = self.RDBParams()
+        self.weightsD, self.biasesD = self.DFFParams()
+        self.weightsU, self.biasesU = self.UPNParams()
+        self.weight_final = tf.Variable(tf.random_normal([self.kernel_size, self.kernel_size, self.c_dim, self.c_dim], stddev=np.sqrt(2.0/9/3)), name='w_f')
+        self.bias_final = tf.Variable(tf.zeros([self.c_dim], name='b_f')),
+        
+        self.pred = self.model()
+        # self.loss = tf.reduce_mean(tf.square(self.labels - self.pred))
+        self.loss = tf.reduce_mean(tf.abs(self.labels - self.pred))
+        self.summary = tf.summary.scalar('loss', self.loss)
+
+        self.model_name = "%s_%s_%s_%s_x%s" % ("rdn", self.D, self.C, self.G, self.scale)
+        self.saver = tf.train.Saver(max_to_keep=10)
+
     def train(self, config):
         print("\nPrepare Data...\n")
         input_setup(config)
         data_dir = get_data_dir(config.checkpoint_dir, config.is_train)
         data_num = get_data_num(data_dir)
+        batch_num = data_num // config.batch_size
 
         images_shape = [None, self.image_size, self.image_size, self.c_dim]
         labels_shape = [None, self.image_size * self.scale, self.image_size * self.scale, self.c_dim]
         self.build_model(images_shape, labels_shape)
-        self.train_op = tf.train.AdamOptimizer(learning_rate=config.learning_rate).minimize(self.loss)
-        tf.global_variables_initializer().run(session=self.sess) 
-        # merged_summary_op = tf.summary.merge_all()
-        # summary_writer = tf.summary.FileWriter(config.checkpoint_dir, self.sess.graph)
 
-        counter = self.load(config.checkpoint_dir)
-        time_ = time.time()
+        counter = self.load(config.checkpoint_dir, restore=False)
+        epoch_start = counter // batch_num
+        batch_start = counter % batch_num
+
+        global_step = tf.Variable(counter, trainable=False)
+        learning_rate = tf.train.exponential_decay(config.learning_rate, global_step, config.lr_decay_steps*batch_num, config.lr_decay_rate, staircase=True)
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        learning_step = optimizer.minimize(self.loss, global_step=global_step)
+
+        tf.global_variables_initializer().run(session=self.sess)
+
+        merged_summary_op = tf.summary.merge_all()
+        summary_writer = tf.summary.FileWriter((os.path.join(config.checkpoint_dir, self.model_name, "log")), self.sess.graph)
+
+        self.load(config.checkpoint_dir, restore=True)
         print("\nNow Start Training...\n")
-        for ep in range(config.epoch):
+        for ep in range(epoch_start, config.epoch):
             # Run by batch images
-            batch_idxs = data_num // config.batch_size
-            for idx in range(0, batch_idxs):
+            for idx in range(batch_start, batch_num):
                 batch_images, batch_labels = get_batch(data_dir, data_num, config.batch_size)
                 counter += 1
 
-                _, err = self.sess.run([self.train_op, self.loss], feed_dict={self.images: batch_images, self.labels: batch_labels})
+                _, err, lr = self.sess.run([learning_step, self.loss, learning_rate], feed_dict={self.images: batch_images, self.labels: batch_labels})
 
                 if counter % 10 == 0:
-                    print("Epoch: [%2d], batch: [%2d/%2d], step: [%2d], time: [%4.4f], loss: [%.8f]" % ((ep+1), idx, batch_idxs, counter, time.time()-time_, err))
-                if counter % 100 == 0:
+                    print("Epoch: [%4d], batch: [%6d/%6d], loss: [%.8f], lr: [%.6f], step: [%d]" % ((ep+1), (idx+1), batch_num, err, lr, counter))
+                if counter % 10000 == 0:
                     self.save(config.checkpoint_dir, counter)
-                    # summary_str = self.sess.run(merged_summary_op)
-                    # summary_writer.add_summary(summary_str, counter)
-                
-                if counter > 0 and counter == batch_idxs * config.epoch:
-                    return
+
+                    summary_str = self.sess.run(merged_summary_op, feed_dict={self.images: batch_images, self.labels: batch_labels})
+                    summary_writer.add_summary(summary_str, counter)
+
+                if counter > 0 and counter == batch_num * config.epoch:
+                    self.save(config.checkpoint_dir, counter)
+                    break
+
+        summary_writer.close()
 
     def eval(self, config):
         print("\nPrepare Data...\n")
@@ -319,7 +334,7 @@ class RDN(object):
             self.build_model(images_shape, labels_shape)
             tf.global_variables_initializer().run(session=self.sess) 
 
-            self.load(config.checkpoint_dir)
+            self.load(config.checkpoint_dir, restore=True)
 
             time_ = time.time()
             result = self.sess.run([self.pred], feed_dict={self.images: input_ / 255.0})
@@ -344,7 +359,7 @@ class RDN(object):
 
             if not os.path.isdir(os.path.join(os.getcwd(),config.result_dir)):
                 os.makedirs(os.path.join(os.getcwd(),config.result_dir))
-            imsave(x[:, :, ::-1], config.result_dir+'/%d.png' % idx)
+            imsave(x[:, :, ::-1], config.result_dir + "/%d.png" % idx)
 
         print("Avg. Time:", avg_time / data_num)
         print("Avg. PSNR:", avg_pasn / data_num)
@@ -366,7 +381,7 @@ class RDN(object):
             self.build_model(images_shape, labels_shape)
             tf.global_variables_initializer().run(session=self.sess) 
 
-            self.load(config.checkpoint_dir)
+            self.load(config.checkpoint_dir, restore=True)
 
             time_ = time.time()
             result = self.sess.run([self.pred], feed_dict={self.images: input_ / 255.0})
@@ -383,35 +398,33 @@ class RDN(object):
 
             if not os.path.isdir(os.path.join(os.getcwd(),config.result_dir)):
                 os.makedirs(os.path.join(os.getcwd(),config.result_dir))
-            imsave(x, config.result_dir+'/%d.png' % idx)
+            imsave(x, config.result_dir + "/%d.png" % idx)
 
         print("Avg. Time:", avg_time / data_num)
 
-    def load(self, checkpoint_dir):
-        print("\nReading Checkpoints.....\n")
-        model_dir = "%s_%s_%s_%s_x%s" % ("rdn", self.D, self.C, self.G, self.scale)
-        checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
+    def load(self, checkpoint_dir, restore):
+        checkpoint_dir = os.path.join(checkpoint_dir, self.model_name)
         ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
- 
+        
         if ckpt and ckpt.model_checkpoint_path:
             ckpt_path = str(ckpt.model_checkpoint_path)
-            self.saver.restore(self.sess, os.path.join(os.getcwd(), ckpt_path))
             step = int(os.path.basename(ckpt_path).split('-')[1])
-            print("\nCheckpoint Loading Success! %s\n" % ckpt_path)
+            if restore:
+                self.saver.restore(self.sess, os.path.join(os.getcwd(), ckpt_path))
+                print("\nCheckpoint Loading Success! %s\n" % ckpt_path)
         else:
             step = 0
-            print("\nCheckpoint Loading Failed! \n")
+            if restore:
+                print("\nCheckpoint Loading Failed! \n")
 
         return step
 
     def save(self, checkpoint_dir, step):
-        model_name = "RDN.model"
-        model_dir = "%s_%s_%s_%s_x%s" % ("rdn", self.D, self.C, self.G, self.scale)
-        checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
+        checkpoint_dir = os.path.join(checkpoint_dir, self.model_name)
 
         if not os.path.exists(checkpoint_dir):
              os.makedirs(checkpoint_dir)
 
         self.saver.save(self.sess,
-                        os.path.join(checkpoint_dir, model_name),
+                        os.path.join(checkpoint_dir, "RDN.model"),
                         global_step=step)
